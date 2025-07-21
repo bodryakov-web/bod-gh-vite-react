8c959375a2507f177a185989bc36e380ca91c9bb
Bodryakov.web <bodryakov.web@gmail.com>
2025-07-21 12:12:40 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bod-gh-vite-react/', // Укажи свой подкаталог
   plugins: [react()],
 })
